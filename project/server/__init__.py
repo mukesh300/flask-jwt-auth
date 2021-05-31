2d108c61ca430aa70ee9e94eed1e7f0b3f4707eb
mukesh300 <mukeshbraj46@gmail.com>
2021-05-31 23:51:02 +0530

    Route setup with register, login and status check

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -5,8 +5,10 @@ import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app_settings = os.getenv(
     'APP_SETTINGS',
@@ -16,3 +18,6 @@ app.config.from_object(app_settings)
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+
+from project.server.auth.views import auth_blueprint
+app.register_blueprint(auth_blueprint)
